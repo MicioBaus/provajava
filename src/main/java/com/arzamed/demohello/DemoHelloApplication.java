5674368784d8f07ef9d34b3b9e96cefef36f954d
alberto <a.ghiribaldi@arzadv.com>
2019-08-22 09:45:34 +0200

    first commit

---
+++ src/main/java/com/arzamed/demohello/DemoHelloApplication.java
@@ -0,0 +1,12 @@
+package com.arzamed.demohello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHelloApplication.class, args);
+    }
+}
